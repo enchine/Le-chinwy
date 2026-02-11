9695faf82ac124467635dea832a2b8138d95d62a
enchine <enchine220@gmail.com>
2026-02-11 20:42:06 +0100

    Add Next.js configuration for image domains

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['via.placeholder.com'], // Mettez ici le domaine de vos images
+  },
+};
+
+module.exports = nextConfig;
